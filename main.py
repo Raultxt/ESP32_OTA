cb9ff9eea1d9ce7d926d579efbb80b3e802bdded
Raultxt <morenoraul427@gmail.com>
2024-11-03 20:30:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ led = machine.Pin(2, machine.Pin.OUT)  # Pin 2 suele ser el LED en el ESP32
 
 while True:
     led.value(1)  # Enciende el LED
-    time.sleep(1)  # Espera 1 segundo
+    time.sleep(5)  # Espera 1 segundo
     led.value(0)  # Apaga el LED
-    time.sleep(1)  # Espera otro segundo
+    time.sleep(5)  # Espera otro segundo
